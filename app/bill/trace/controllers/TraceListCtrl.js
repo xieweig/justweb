e4811a11eea1b51a3fdcad815aa789167be791b3
gaoshang <25012061@qq.com>
2017-12-23 15:45:57 +0800

    增加运单列表页面

---
+++ app/bill/trace/controllers/TraceListCtrl.js
@@ -0,0 +1,125 @@
+'use strict';
+
+angular.module('app').controller('TraceListCtrl', function ($scope, $uibModal) {
+    $scope.params = {};
+    // 编辑窗口的对象
+    $scope.traceEdit = {};
+    /**
+     * 搜索
+     */
+    $scope.search = function () {
+        $scope.traceGrid.kendoGrid.dataSource.page(1);
+    };
+    $scope.traceGrid = {
+        url: '/api/baseInfo/cargo/findByCondition',
+        params: $scope.params,
+        kendoSetting: {
+            autoBind: false,
+            pageable: true,
+            columns: [
+                { selectable: true },
+                { command: [{ name: 's', text: "查看" }, { name: 'e', text: "修改" }, { name: 't', text: "收货" }], title: "操作", width: 180 },
+                { field: "sn", title: "物流公司", width: 120 },
+                { field: "inStationName", title: "运单单号", width: 120 },
+                { field: "inStationName", title: "出库站点", width: 120 },
+                { field: "inStationName", title: "入库站点", width: 120 },
+                { field: "inStationName", title: "发货时间", width: 120 },
+                { field: "inStationName", title: "录单时间", width: 120 },
+                { field: "inStationName", title: "录单人", width: 120 },
+                { field: "inStationName", title: "运送件数", width: 120 },
+                { field: "inStationName", title: "运单状态", width: 120 }
+            ]
+        }
+    };
+
+    /**
+     * 增加跟踪信息
+     */
+    $scope.addTrace = function () {
+        // 初始化参数
+        $scope.traceEdit.trace = {};
+        openEditTraceModal();
+    };
+
+    /**
+     * 打开trace编辑窗口
+     */
+    function openEditTraceModal() {
+        initTraceEdit();
+        $scope.traceEditModal = $uibModal.open({
+            templateUrl: 'app/bill/trace/modals/traceEdit.html',
+            scope: $scope,
+            size: 'lg'
+        });
+    }
+
+    /**
+     * 初始化
+     */
+    function initTraceEdit() {
+        $scope.traceEdit.detailsGrid = {
+            primaryId: 'sn',
+            kendoSetting: {
+                editable: true,
+                autoBind: false,
+                persistSelection: true,
+                columns: [
+                    { selectable: true },
+                    { field: "sn", title: "出库单号", width: 120 },
+                    { field: "inStationName", title: "所属包号", width: 120 },
+                    { field: "inStationName", title: "出库站点", width: 120 },
+                    { field: "inStationName", title: "入库站点", width: 120 },
+                    { field: "inStationName", title: "出库时间", width: 120 },
+                    { field: "inStationName", title: "录单人", width: 120 },
+                    { field: "inStationName", title: "品种数", width: 120 },
+                    { field: "inStationName", title: "货物数", width: 120 },
+                    { command: [{ name: 'destroy', text: "删除" }], title: "操作", width: 155 }
+                ]
+            }
+        };
+
+        /**
+         * 打开增加货单明细modal
+         */
+        $scope.traceEdit.showAddModal = function () {
+            $scope.traceEdit.currentDetails = { sn: 1 };
+            $scope.traceEdit.addModal = $uibModal.open({
+                templateUrl: 'app/bill/trace/modals/traceDetails.html',
+                scope: $scope,
+                size: 'lg'
+            });
+        }
+
+        /**
+         * 增加一条明细
+         * @param {*是否点击的下一条 如果点击的下一条需要清空表格数据} isNext 
+         */
+        $scope.traceEdit.addDetails = function (isNext) {
+            if (!$scope.traceEdit.currentDetails.sn) {
+                swal('请输入出库单号', '', 'warning');
+                return;
+            }
+            var dataSource = $scope.traceEdit.detailsGrid.kendoGrid.dataSource;
+            var repeatIndex = _.findIndex(dataSource.data(), function (item) {
+                return item.sn == $scope.traceEdit.currentDetails.sn;
+            });
+            if (repeatIndex >= 0) {
+                swal('该出库单号已存在', '', 'warning');
+                return true;
+            }
+            dataSource.add($scope.traceEdit.currentDetails);
+            $scope.currentDetails = {};
+            if (!isNext) {
+                $scope.traceEdit.addModal.close();
+            }
+        }
+
+        /**
+         * 删除
+         */
+        $scope.traceEdit.deleteDetails = function () {
+            var selectIds = $scope.traceEdit.detailsGrid.kendoGrid.selectedKeyNames();
+            alert('将要删除' + selectIds);
+        }
+    }
+});
--- app/bill/trace/module.js
+++ app/bill/trace/module.js
@@ -9,6 +9,18 @@ angular.module('app.bill.trace', ['ui.router']).config(function ($stateProvider)
                 title: '运单跟踪'
             }
         })
+        .state('app.bill.trace.list', {
+            url: '/bill/trace/list',
+            data: {
+                title: '添加运单'
+            },
+            views: {
+                "content@app": {
+                    templateUrl: 'app/bill/trace/views/list.html',
+                    controller: 'TraceListCtrl'
+                }
+            }
+        })
         .state('app.bill.trace.add', {
             url: '/bill/trace/add',
             data: {
