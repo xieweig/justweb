55d0a353aab79a6f7ec42fa0c50d96469b5022fb
gaoshang <25012061@qq.com>
2018-01-03 17:21:13 +0800

    增加新增页面

--- app/bill/plan/controllers/PlanAddCtrl.js
+++ app/bill/plan/controllers/PlanAddCtrl.js
@@ -1,25 +1,48 @@
 'use strict';
 
-angular.module('app').controller('PlanAddCtrl', function ($scope) {
+angular.module('app').controller('PlanAddCtrl', function ($scope, $timeout) {
+
+    $timeout(function () {
+        var isTrigger = false;
+        $('.nav-tabs a').click(function () {
+            var _this = this;
+            if (!isTrigger) {
+                swal({
+                    title: '该操作将会清空原有的数据,确定继续?',
+                    showCancelButton: true,
+                    type: "warning"
+                }).then(function (result) {
+                    if (!result.value) {
+                        isTrigger = true;
+                        $(_this).parent().siblings().find('a').trigger('click');
+                    }
+                });
+            } else {
+                // 程序只会触发一次
+                isTrigger = false;
+            }
+        });
+    });
 
     $('#grid').on('click', '.kendo-btn-a', function () {
 
     })
 
     // 项目数组
-    $scope.itemMap = [];
-    $scope.addItem = function () {
+    $scope.cargoMap = [];
+    $scope.materialMap = [];
+    $scope.addItem = function (type) {
         var item = {
             unfurled: true,
             cargo: {},
             stationGrid: {
                 primaryId: 'stationCode',
-                params: $scope.kendoQueryCondition,
                 kendoSetting: {
                     height: 150,
                     editable: true,
                     autoBind: false,
                     columns: [
+                        { command: [{ name: 'destroy', text: "删除" }], title: "操作", width: 155, locked: true },
                         { field: "inStationName", title: "调出站点" },
                         { field: "outStationName", title: "调入站点" },
                         {
@@ -27,59 +50,71 @@ angular.module('app').controller('PlanAddCtrl', function ($scope) {
                                 return '<a class="kendo-btn-a"></a>';
                             }
                         },
-                        { field: "number", title: "调剂数量(点击修改)", editable: true },
-                        { command: [{ name: 'destroy', text: "删除" }], title: "操作", width: 155, locked: true }
+                        { field: "number", title: "调剂数量(点击修改)", editable: true }
                     ]
                 }
             }
         }
-        $scope.itemMap.push(item);
+        if (type === 'material') {
+            $scope.materialMap.push(item);
+        } else {
+            $scope.cargoMap.push(item);
+        }
     };
 
     // 伸缩项
-    $scope.scaling = function (index) {
-        $scope.itemMap[index].unfurled = !$scope.itemMap[index].unfurled;
+    $scope.scaling = function (item, index) {
+        item.unfurled = !item.unfurled;
     };
 
     // 添加货物
-    $scope.addCargo = function (index) {
-        $scope.itemMap[index].cargo = {
-            cargoName: '货物',
-            cargoCode: 'CODE001',
-            barCode: '1564646465',
-            selfBarCode: '1564646465',
-            materialName: '咖啡豆',
-            number: '100',
-            measurementName: 'g/包',
-            class: '分类'
-        };
+    $scope.addCargo = function (item, type) {
+        if (type === 'material') {
+            item.material = {
+                materialName: '原料',
+                materialCode: 'CODE001',
+            };
+        } else {
+            item.cargo = {
+                cargoName: '货物',
+                cargoCode: 'CODE001',
+                barCode: '1564646465',
+                selfBarCode: '1564646465',
+                materialName: '咖啡豆',
+                number: '100',
+                measurementName: 'g/包',
+                class: '分类'
+            };
+        }
     };
 
     // 删除货物
-    $scope.removeCargo = function (index) {
-        $scope.itemMap.splice(index, 1);
+    $scope.removeCargo = function (type, index) {
+        if (type === 'material') {
+            $scope.materialMap.splice(index, 1);
+        } else {
+            $scope.cargoMap.splice(index, 1);
+        }
     }
 
     // 清空货物
-    $scope.clearCargo = function (index) {
-        $scope.itemMap[index].cargo = {};
+    $scope.clearCargo = function (item, index) {
+        item.cargo = {};
     }
 
     // 添加站点
-    $scope.addStation = function (index) {
-        var item = {
+    $scope.addStation = function (item, index) {
+        item.stationGrid.kendoGrid.dataSource.add({
             stationCode: generateMixed(10),
             inStationName: '调入站点',
             outStationName: '调出站点',
             number: '0'
-        };
-        $scope.itemMap[index].stationGrid.kendoGrid.dataSource.add(item);
+        });
     };
 
 
     // 清空站点
-    $scope.clearStation = function (index) {
-        var item = $scope.itemMap[index];
+    $scope.clearStation = function (item, index) {
         item.stationGrid.kendoGrid.dataSource.data([]);
     };
 });
