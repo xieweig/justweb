81aedeb96b4785095d5faf1c1f3e23b1a82dcb51
gaoshang <25012061@qq.com>
2018-01-10 12:13:06 +0800

    增加配送

--- app/bill/delivery/controllers/DeliveryListCtrl.js
+++ app/bill/delivery/controllers/DeliveryListCtrl.js
@@ -37,7 +37,7 @@ angular.module('app').controller('DeliveryListCtrl', function ($scope, $uibModal
             autoBind: false,
             pageable: true,
             columns: [
-                {title: "操作", width: 180, locked: true, command: [{name: 'picking', text: "拣货", click: picking}, {name: 'picking', text: "查看", click: lookDetails}]},
+                {title: "操作", width: 180, locked: true, command: [{name: 'picking', text: "拣货", click: picking}, {name: 'look', text: "查看", click: lookDetails}]},
                 {field: "xxxxx", title: "完成率", width: 120},
                 {field: "xxxxx", title: "站点计划号", width: 120},
                 {field: "xxxxx", title: "录单时间", width: 120},
@@ -55,6 +55,28 @@ angular.module('app').controller('DeliveryListCtrl', function ($scope, $uibModal
     function picking(e) {
         e.preventDefault();
         var dataItem = this.dataItem($(e.currentTarget).closest("tr"));
+        $scope.planDetails = {billCode: '111111111'};
+        $scope.planDetailsGrid = {
+            kendoSetting: {
+                dataSource: [{}, {}],
+                columns: [
+                    {field: "xxxxx", title: "货物名称", width: 120},
+                    {field: "xxxxx", title: "货物编码", width: 120},
+                    {field: "xxxxx", title: "所属原料", width: 120},
+                    {field: "xxxxx", title: "规格", width: 120},
+                    {field: "xxxxx", title: "应拣数量", width: 120},
+                    {field: "xxxxx", title: "调入站点", width: 120},
+                    {field: "xxxxx", title: "数量", width: 120},
+                    {field: "xxxxx", title: "规格品种", width: 120},
+                    {field: "xxxxx", title: "备注", width: 120}
+                ]
+            }
+        };
+        $uibModal.open({
+            templateUrl: 'app/bill/delivery/modals/planPick.html',
+            controller: 'DeliveryPlanPickCtrl',
+            size: 'lg'
+        });
     }
 
     // 查看详情
