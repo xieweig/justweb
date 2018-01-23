4d27c7cbd3ce5913d5865511f8bdf5bed2851de3
gaoshang <25012061@qq.com>
2018-01-23 10:53:29 +0800

    修复BUG

--- app/_common/expand/directives/confineMoney.js
+++ app/_common/expand/directives/confineMoney.js
@@ -7,14 +7,7 @@ angular.module('app').directive('confineMoney', function ($uibModal) {
             ngModel: '='
         },
         link: function (scope, elm, attr) {
-            elm.blur(function () {
-                var value = parseFloat(this.value);
-                value = value < 0 ? -value : value;
-                value = value > 9999999.99 ? 0 : value;
-                scope.$apply(function () {
-                    scope.ngModel = value === value ? value : '';
-                });
-            });
+            inputDecimal(elm, scope);
         }
     }
 });
