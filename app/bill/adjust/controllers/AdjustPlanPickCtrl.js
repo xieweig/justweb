0bb6f77f4fe0c334ec886fd04fa4d5073c6a68e0
gaoshang <25012061@qq.com>
2018-01-25 14:44:08 +0800

    制作调剂

--- app/bill/adjust/controllers/AdjustPlanPickCtrl.js
+++ app/bill/adjust/controllers/AdjustPlanPickCtrl.js
@@ -2,50 +2,122 @@
 
 angular.module('app').controller('AdjustPlanPickCtrl', function ($scope, $uibModal, Common, params, ApiService, $timeout) {
     $scope.bill = params.bill;
+    $scope.pickType = 'material';
 
     $timeout(function () {
         if ($.cookie('currentStationType') === 'LOGISTICS') {
+            $scope.bill.outStorageCode = 'STORAGE';
             $('#outStorageCode').val('STORAGE').trigger('change');
         } else {
+            $scope.bill.outStorageCode = 'NORMAL';
             $('#outStorageCode').val('NORMAL').trigger('change');
         }
     });
-    // 初始化原料
-    $scope.materialList = [];
-    _.each($scope.bill.childPlanBillDetails, function (item) {
-        $scope.materialList.push({
-            material: {
-                amount: item.amount,
-                actualAmount: 0,
-                materialName: item.rawMaterial.rawMaterialName,
-                materialCode: item.rawMaterial.rawMaterialCode
-            },
-            kendoGrid: {
-                kendoSetting: {
-                    editable: true,
-                    columns: [
-                        {title: "操作", width: 85, command: [{name: 'del', text: "删除", click: delCargo}]},
-                        {field: "cargoName", title: "货物名称", width: 120},
-                        {field: "cargoCode", title: "货物编码", width: 120},
-                        {field: "rawMaterialName", title: "所属原料", width: 120},
-                        {
-                            title: "规格", width: 120,
-                            template: function (data) {
-                                return data.number + getTextByVal(params.cargoUnit, data.measurementCode);
-                            }
-                        },
-                        {field: "actualAmount", title: "实拣数量", width: 120, kType: 'number', editable: true}
-                    ],
-                    save: function (e) {
-                        $timeout(function () {
-                            calculateActual();
-                        });
-                    }
+    // 库位切换提示
+    $scope.$watch('bill.outStorage', function (newVal, oldVal) {
+        if (oldVal && newVal !== oldVal && newVal !== $scope.bill.outStorageCode) {
+            swal({
+                title: '是否将出库库位修改为' + getTextByVal($scope.outType, newVal),
+                type: 'warning',
+                confirmButtonText: '是的',
+                showCancelButton: true
+            }).then(function (res) {
+                if (res.value) {
+                    $scope.bill.outStorageCode = newVal;
+                } else if (res.dismiss === 'cancel') {
+                    // 重置选项为初始
+                    $('#outStorageCode').val(oldVal).trigger('change');
                 }
-            }
-        });
+            });
+        }
     });
 
+    // 货物grid
+    $scope.cargoGrid = {
+        primaryId: 'cargoCode',
+        kendoSetting: {
+            dataSource: [],
+            columns: [
+                {field: "cargoName", title: "货物名称", width: 120},
+                {field: "cargoCode", title: "货物编码", width: 120},
+                {field: "rawMaterialName", title: "所属原料", width: 120},
+                {
+                    field: "measurementCode", title: "规格", width: 120,
+                    template: function (data) {
+                        return data.number + getTextByVal(params.cargoUnit, data.measurementCode);
+                    }
+                },
+                {field: "shippedAmount", title: "应拣数量", width: 120},
+                {field: "actualAmount", title: "实拣数量", width: 120}
+            ]
+        }
+    };
+    $scope.cargoArray = [];
+    (function () {
+        if (params.bill.basicEnum === 'BY_CARGO') {
+            var cargoCodes = _.map(params.bill.childPlanBillDetails, function (item) {
+                return item.rawMaterial.cargo.cargoCode;
+            });
+            Common.getCargoByCodes(cargoCodes).then(function (cargoList) {
+                var cargoObject = _.zipObject(_.map(cargoList, function (item) {
+                    return item.cargoCode
+                }), cargoList);
+                _.each(params.bill.childPlanBillDetails, function (item) {
+                    var cargo = cargoObject[item.rawMaterial.cargo.cargoCode];
+                    $scope.cargoArray.push({
+                        cargoName: cargo.cargoName,
+                        cargoCode: cargo.cargoCode,
+                        rawMaterialCode: cargo.rawMaterialCode,
+                        rawMaterialName: cargo.rawMaterialName,
+                        rawMaterialTypeName: cargo.rawMaterialTypeName,
+                        measurementCode: cargo.measurementCode,
+                        number: cargo.number,
+                        shippedAmount: item.amount,
+                        actualAmount: 0
+                    });
+                });
+                $timeout(function () {
+                    $scope.cargoGrid.kendoGrid.dataSource.data($scope.cargoArray);
+                });
+            });
+        }
+
+        // 初始化原料
+        $scope.materialList = [];
+        _.each($scope.bill.childPlanBillDetails, function (item) {
+            $scope.materialList.push({
+                material: {
+                    amount: item.amount,
+                    actualAmount: 0,
+                    materialName: item.rawMaterial.rawMaterialName,
+                    materialCode: item.rawMaterial.rawMaterialCode
+                },
+                kendoGrid: {
+                    kendoSetting: {
+                        editable: true,
+                        columns: [
+                            {title: "操作", width: 85, command: [{name: 'del', text: "删除", click: delCargo}]},
+                            {field: "cargoName", title: "货物名称", width: 120},
+                            {field: "cargoCode", title: "货物编码", width: 120},
+                            {field: "rawMaterialName", title: "所属原料", width: 120},
+                            {
+                                title: "规格", width: 120,
+                                template: function (data) {
+                                    return data.number + getTextByVal(params.cargoUnit, data.measurementCode);
+                                }
+                            },
+                            {field: "actualAmount", title: "实拣数量", width: 120, kType: 'number', editable: true}
+                        ],
+                        save: function (e) {
+                            $timeout(function () {
+                                calculateActual();
+                            });
+                        }
+                    }
+                }
+            });
+        });
+    }());
     function calculateActual() {
         _.each($scope.materialList, function (material) {
             material.material.actualAmount = 0;
@@ -106,22 +178,6 @@ angular.module('app').controller('AdjustPlanPickCtrl', function ($scope, $uibMod
         });
     }
 
-
-    // 货物grid
-    $scope.cargoGrid = {
-        kendoSetting: {
-            dataSource: [{xxxxx: 123, a: 123, b: 44}, {xxxxx: 123, a: 123, b: 44}],
-            columns: [
-                {field: "xxxxx", title: "货物名称", width: 120},
-                {field: "xxxxx", title: "货物编码", width: 120},
-                {field: "xxxxx", title: "所属原料", width: 120},
-                {field: "xxxxx", title: "规格", width: 120},
-                {field: "xxxxx", title: "应拣数量", width: 120},
-                {field: "b", title: "实拣数量", width: 120}
-            ]
-        }
-    };
-
     // 删除原料表格中的货物
     function delCargo(e) {
         e.preventDefault();
@@ -142,12 +198,46 @@ angular.module('app').controller('AdjustPlanPickCtrl', function ($scope, $uibMod
     $scope.enterCargo = function (e) {
         var keyCode = window.event.keyCode;
         if (keyCode === 13) {
-            $uibModal.open({
-                templateUrl: 'app/bill/adjust/modals/planPickCargo.html',
-                size: 'md',
-                scope: $scope
+            if (!$scope.cargoBarcode) {
+                swal('请输入货物条码', '', 'warning');
+                return;
+            }
+            Common.getCargoByBarCode($scope.cargoBarcode).then(function (cargo) {
+                if (!cargo) {
+                    swal('货物不存在', '', 'warning');
+                    return;
+                }
+                $scope.cargoByBarcode = cargo;
+                $scope.cargoByBarcode.range = 'add';
+                $uibModal.open({
+                    templateUrl: 'app/bill/adjust/modals/planPickCargo.html',
+                    size: 'md',
+                    scope: $scope
+                }).closed.then(function () {
+                    $scope.cargoByBarcode = null;
+                    $scope.cargoBarcode = '';
+                });
             });
         }
+    };
+
+    // 获取货物加减的范围
+    $scope.getCargoRange = function () {
+        var dataItem = _.find($scope.cargoArray, function (item) {
+            return item.cargoCode === $scope.cargoByBarcode.cargoCode;
+        });
+        var number = 0;
+        var actualAmount = parseInt(dataItem.actualAmount);
+        if ($scope.cargoByBarcode.range === 'add') {
+            number = parseInt($scope.cargoByBarcode.addNumber);
+        } else {
+            number = 0 - parseInt($scope.cargoByBarcode.lessNumber);
+        }
+        if (number) {
+            dataItem.actualAmount = actualAmount + number;
+        }
+        $scope.cargoGrid.kendoGrid.dataSource.data($scope.cargoArray);
+
     };
 
     // 保存拣货
@@ -182,7 +272,7 @@ angular.module('app').controller('AdjustPlanPickCtrl', function ($scope, $uibMod
         }
     };
 
-    function getParams() {
+    function getParams(type) {
         var result = {
             sourceCode: params.bill.billCode,
             basicEnum: 'BY_MATERIAL',
@@ -224,20 +314,12 @@ angular.module('app').controller('AdjustPlanPickCtrl', function ($scope, $uibMod
             ]
         };
         result.billDetails = [];
-        var emptyItem = _.find($scope.materialList, function (material) {
-            var dataSource = material.kendoGrid.kendoGrid.dataSource.data();
-            if (dataSource.length === 0) {
-                swal('请选择' + material.material.materialName + '的货物', '', 'error');
-                return true;
-            }
-            var emptyCargo = _.find(dataSource, function (dataItem) {
-                if (!dataItem.actualAmount && dataItem.actualAmount !== 0) {
-                    swal('请选择' + material.materialName + '中' + dataItem.cargoName + '的实拣数量', '', 'error');
-                    return true;
-                }
+        var emptyItem = null;
+        if ($scope.pickType === 'cargo') {
+            emptyItem = _.find($scope.cargoArray, function (dataItem) {
                 result.billDetails.push({
                     actualAmount: dataItem.actualAmount,
-                    shippedAmount: material.amount,
+                    shippedAmount: dataItem.shippedAmount,
                     belongMaterialCode: dataItem.rawMaterialCode,
                     rawMaterial: {
                         cargo: {
@@ -250,11 +332,39 @@ angular.module('app').controller('AdjustPlanPickCtrl', function ($scope, $uibMod
                 });
                 return false;
             });
-            if (!emptyCargo) {
-                return false;
-            }
-            return true;
-        });
+        } else {
+            emptyItem = _.find($scope.materialList, function (material) {
+                var dataSource = material.kendoGrid.kendoGrid.dataSource.data();
+                if (type === 'submit' && dataSource.length === 0) {
+                    swal('请选择' + material.material.materialName + '的货物', '', 'error');
+                    return true;
+                }
+                var emptyCargo = _.find(dataSource, function (dataItem) {
+                    if (type === 'submit' && !dataItem.actualAmount && dataItem.actualAmount !== 0) {
+                        swal('请选择' + material.material.materialName + '中' + dataItem.cargoName + '的实拣数量', '', 'error');
+                        return true;
+                    }
+                    result.billDetails.push({
+                        actualAmount: dataItem.actualAmount,
+                        shippedAmount: material.amount,
+                        belongMaterialCode: dataItem.rawMaterialCode,
+                        rawMaterial: {
+                            cargo: {
+                                cargoCode: dataItem.cargoCode,
+                                cargoName: dataItem.cargoName
+                            },
+                            rawMaterialCode: dataItem.rawMaterialCode,
+                            rawMaterialName: dataItem.rawMaterialName
+                        }
+                    });
+                    return false;
+                });
+                if (!emptyCargo) {
+                    return false;
+                }
+                return true;
+            });
+        }
         if (!emptyItem) {
             return result;
         }
