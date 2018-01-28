6d1f639b64975a739a81900cda64d9408640022f
zhangzenan <zhangzenan@sisyphe.cn>
2018-01-28 17:51:47 +0800

    fix typo

--- app/bill/restock/controllers/RestockTransferSearchCtrl.js
+++ app/bill/restock/controllers/RestockTransferSearchCtrl.js
@@ -47,12 +47,12 @@ angular.module('app').controller('RestockTransferSearchCtrl', function ($scope, 
                 {field: "operatorName", title: "操作人", width: 60},
                 {
                     title: "入库单出库站点", width: 150, template: function (data) {
-                        return getTextByVal($scope.station, data.outLocation.stationCode)
+                        return getTextByVal($scope.station, data.inStorageBillOutStationCode)
                     }
                 },
                 {
                     title: "入库单入库站点", width: 150, template: function (item) {
-                        return getTextByVal($scope.station, item.inLocation.stationCode)
+                        return getTextByVal($scope.station, item.inStorageBillInStationCode)
                     }
                 },
                 {title: "调拨单调出库位", width: 120, template: function (data) {
@@ -63,7 +63,7 @@ angular.module('app').controller('RestockTransferSearchCtrl', function ($scope, 
                     }},
                 {field: "totalAmount", title: "调拨数量", width: 60},
                 {field: "totalVarietyAmount", title: "调拨品种", width: 60},
-                {field: "totalPrice", title: "总进价", width: 60}
+                // {field: "totalPrice", title: "总进价", width: 60}
             ]
         }
     };
